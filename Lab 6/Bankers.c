fcfa08843b31b71a292e5084b4cc04cab07369db
Paarth Batra <138647078+hydro-7@users.noreply.github.com>
2025-03-18 14:38:18 +0530

    Update Bankers.c

--- Lab 6/Bankers.c
+++ Lab 6/Bankers.c
@@ -1,37 +1,83 @@
 #include<stdio.h>
-int main() {
-    int n; scanf("%d", &n);
-    int allocation[3][n], max[3][n], available[3], need[3][n];
+#include<stdbool.h>
+#include<stdlib.h>
+#include<string.h>
 
-    for(int i = 0; i < n; i++) scanf("%d %d %d", &allocation[0][i], &allocation[1][i], &allocation[2][i]);
-    for(int i = 0; i < n; i++) scanf("%d %d %d", &max[0][i], &max[1][i], &max[2][i]);
-    scanf("%d %d %d", &available[0], &available[1], &available[2]);
+int main(){
+    int n,m;
+    scanf("%d %d",&n,&m);
     
-    for(int i = 0; i < n; i++) {
-        need[0][i] = max[0][i] - allocation[0][i];
-        need[1][i] = max[1][i] - allocation[1][i];
-        need[2][i] = max[2][i] - allocation[2][i];
+    int alloc[n][m];
+    int max[n][m];
+    int available[m];
+    int need[n][m];
+    bool check[m];
+    
+    for(int i=0;i<m;i++) check[i]=false;
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            scanf("%d",&alloc[i][j]);
+        }
     }
-
-    int flag = 1, cnt = 0;
-
-    while(flag && (cnt < 100))
-    {   
-        for(int i = 0; i < n; i++) {
-            if(available[0] >= need[0][i] && available[1] >= need[1][i] && available[2] >= need[2][i]) {
-                available[0] += allocation[0][i];
-                available[1] += allocation[1][i];
-                available[2] += allocation[2][i];
-
-                need[0][i] = 1000, need[1][i] = 1000, need[2][i] = 1000;
+    
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            scanf("%d",&max[i][j]);
+        }
+    }
+    
+    for(int i=0;i<m;i++){
+        scanf("%d",&available[i]);
+    }
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            need[i][j]=max[i][j]-alloc[i][j];
+        }
+    }
+    
+    
+    while(1){
+        int find=0;
+        for(int i=0;i<n;i++){
+            int flag=1;
+            if(!check[i]){
+                for(int j=0;j<m;j++){
+                    if(available[m]<need[i][j]){
+                        flag=0;
+                        break;
+                    }
+                }
+            }
+            if(flag){
+                check[i]=true;
+                find=1;
+                for(int k=0;k<m;k++){
+                    available[k]+=alloc[i][k];
+                }
+                break;
             }
         }
-
-        flag = 0;
-        for(int i = 0; i < n; i++) if(need[0][i] != 1000) flag = 1;
-        cnt++;
-    }
-
-    if(cnt == 100) printf("Unsafe\n");
-    else printf("Safe\n");
+        
+        if(!find){
+            printf("Safe\n");
+            break;
+        }
+        
+        bool all=true;
+        for(int i=0;i<m;i++){
+            if(!check[i]){
+                all=false;
+                break;
+            }
+        }
+        if(all){
+            printf("UnSafe\n");
+            break;
+        }
+    
+    }   
+    return 0;
 }
